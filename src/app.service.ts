bb272a701f6eef0e345a99a9c1ee40b17c3b06b2
toanle312 <toanle3122002@gmail.com>
2024-03-31 10:20:07 +0700

    Show response when open API link

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,10 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  getHello() {
+    return {
+      statusCode: '200',
+      message: 'Your API is ready to use',
+    };
   }
 }
