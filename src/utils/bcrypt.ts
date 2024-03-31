52f32825d06602c40ae1d9b26a89d2b54a0fde3a
toanle312 <toanle3122002@gmail.com>
2024-03-31 09:55:57 +0700

    Add domain into whitelist domain

--- src/utils/bcrypt.ts
+++
@@ -1,9 +0,0 @@
-import * as bcrypt from 'bcrypt';
-
-export const encodeEmail = (email: string) => {
-  try {
-    return bcrypt.hashSync(email, 10);
-  } catch (error) {
-    console.log(error);
-  }
-};
--- src/utils/contant.ts
+++ src/utils/contant.ts
@@ -1,1 +1,1 @@
-export const WHITELIST_DOMAINS = [];
+export const WHITELIST_DOMAINS = ['https://g-weather-forecast.vercel.app'];
